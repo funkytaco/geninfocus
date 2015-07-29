374b497d7113bc7f2989d6a8389a4cdbe8e4dffd
Luis Gonzalez <decaturdev@gmail.com>
2015-07-29 05:07:47 -0400

    Registration template - begin porting old template to MEAN.io (incomplete)

--- packages/custom/registration/public/controllers/registration.js
+++ packages/custom/registration/public/controllers/registration.js
@@ -1,11 +1,22 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.registration').controller('RegistrationController', ['$scope', 'Global', 'Registration',
-  function($scope, Global, Registration) {
+angular.module('mean.registration').controller('RegistrationController', ['$scope', 'Global', 'MeanUser', 'Registration',
+  function($scope, Global, MeanUser, Registration) {
     $scope.global = Global;
     $scope.package = {
       name: 'registration'
     };
+
+    //$scope.test = 'Testing 123';
+    var vm = this;
+
+    vm.registrationVars = {
+      authenticated: MeanUser.loggedin,
+      user: MeanUser.user,
+      isAdmin: MeanUser.isAdmin
+    };
+
+
   }
 ]);
